f6256727469b06b44b83ceec2501e956117cdf43
unknown <zydarr5@gmail.com>
2021-04-09 18:05:51 +0200

    Angularjs hozzáadása és minta működés létrehozása.

--- gruntfile.js
+++ gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
         build: {
           src: [
               'node_modules/jquery/dist/jquery.min.js',
+              'node_modules/angular/angular.min.js',
               'node_modules/bootstrap/dist/js/bootstrap.min.js',
               'js/**/*.js'
             ],
