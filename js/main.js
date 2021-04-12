ca72e7bd3354133c3e3b1c7fda8202596eb091db
unknown <zydarr5@gmail.com>
2021-04-12 15:43:12 +0200

    Egyszerű http kérés

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 var reloaded = angular.module( "reloaded", [] );
-reloaded.controller( "hello", ['$scope', function($scope){
-    $scope.name = "Jeffrey";
-}] );
+reloaded.controller( "hello", ['$scope', '$http', 
+    function($scope, $http){
+        $scope.name = "Jeffrey";
+
+        $scope.users = [];
+        /* $http.get('json/user.json')
+            .then( function(data){
+                console.log(data);
+        }); */
+        console.log("Itt");
+    }] 
+);
