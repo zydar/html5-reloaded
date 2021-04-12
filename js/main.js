a17b75d4da06cbacb7f76a56b53545924bac233c
unknown <zydarr5@gmail.com>
2021-04-12 17:31:37 +0200

    Frissítés

--- js/main.js
+++ js/main.js
@@ -3,7 +3,36 @@ reloaded.controller( "hello", ['$scope', '$http',
     function($scope, $http){
         $scope.name = "Jeffrey";
 
-        $scope.users = [];
+        $scope.users = [{
+            "name": "Jeffrey",
+            "age": 42,
+            "address": "New York",
+            "job": "programmer"
+        },
+        {
+            "name": "Joe",
+            "age": 16,
+            "address": "New York",
+            "job": "programmer"
+        },
+        {
+            "name": "Jack",
+            "age": 38,
+            "address": "New York",
+            "job": "programmer"
+        },
+        {
+            "name": "Zsuzsi",
+            "age": 54,
+            "address": "New York",
+            "job": "programmer"
+        },
+        {
+            "name": "Józsi",
+            "age": 22,
+            "address": "New York",
+            "job": "programmer"
+        }];
         /* $http.get('json/user.json')
             .then( function(data){
                 console.log(data);
