74fc6d8e0f5dd35c2fe3b4b1e2a6083fc56a7aca
unknown <zydarr5@gmail.com>
2021-04-20 18:04:57 +0200

    Ajax

--- src/js/main.js
+++ src/js/main.js
@@ -2,41 +2,11 @@ var reloaded = angular.module( "reloaded", [] );
 reloaded.controller( "hello", ['$scope', '$http', 
     function($scope, $http){
         $scope.name = "Jeffrey";
-
-        $scope.users = [{
-            "name": "Jeffrey",
-            "age": 42,
-            "address": "New York",
-            "job": "programmer"
-        },
-        {
-            "name": "Joe",
-            "age": 16,
-            "address": "New York",
-            "job": "programmer"
-        },
-        {
-            "name": "Jack",
-            "age": 38,
-            "address": "New York",
-            "job": "programmer"
-        },
-        {
-            "name": "Zsuzsi",
-            "age": 54,
-            "address": "New York",
-            "job": "programmer"
-        },
-        {
-            "name": "Józsi",
-            "age": 22,
-            "address": "New York",
-            "job": "programmer"
-        }];
-        /* $http.get('json/user.json')
+        $http.get('json/user.json')
             .then( function(data){
+                $scope.users = data;
                 console.log(data);
-        }); */
+        });
         console.log("Itt");
     }] 
 );
